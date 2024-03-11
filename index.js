f0b4955ffcecd9dcb2000c4143ee67da73538d40
Emilse Arias <emilsearias@gmail.com>
2024-03-11 17:18:41 -0300

    seleccionamos un puerto para correr el backend

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 import express from 'express';
 
-console.log('hola mundo 3')
-console.log('hola mundo sdfjsdfhsdf kjhk')
-
 //1- configurar un puerto
-
+const app = express();
+app.set('port', process.env.PORT || 4000);
+app.listen(app.get('port'), ()=>{
+    console.log('Estoy en el puerto '+app.get('port'))
+})
 //2- configurar los middlewares
 
 //3- configurar las rutas
