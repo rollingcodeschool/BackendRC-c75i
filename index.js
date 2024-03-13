0e38b5a6fd029d767cb4de95d909834af03fab4e
Emilse Arias <emilsearias@gmail.com>
2024-03-13 16:55:24 -0300

    agregamos el midleware static para cargar el index.html en la ruta principal del servidor

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ import 'dotenv/config'; //permite procesar variables de entorno
 import cors from 'cors';
 import morgan from 'morgan';
 import productosRouter from './src/routes/productos.routes.js';
+import path from 'path';
+import { fileURLToPath } from 'url';
 
 //1- configurar un puerto
 const app = express();
@@ -15,11 +17,14 @@ app.use(cors()); //permite conexiones remotas
 app.use(morgan('dev')); //nos da informacion extra en la terminal
 app.use(express.json()); //permite interpretar los datos en formato json
 app.use(express.urlencoded({extended:true})); //ayuda a intepretar los datos del body del request
-// todo: configurar index.html
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+app.use(express.static(path.join(__dirname, '/public')))
 
 //3- configurar las rutas
 // app.get('/nuevo/producto',(req, res)=>{
 //  console.log('aqui obtener la lista de todos los productos');
 //  res.send('Aqui enviaremos la lista de productos')
 // })
+// http://localhost:4001/api/nuevo/producto
 app.use('/api', productosRouter)
